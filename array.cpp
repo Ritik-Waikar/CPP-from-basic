1c02e20c872d777f0687791c9820566ea902a524
Ritik-Waikar <rwcool14@gmail.com>
2026-09-08 14:49:59 +0530

    Trial

--- array.cpp
+++ array.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main(){
    // Data structure that can hold multiple values accessed by index numbers
-   //string name = "Ritik";
-   // this is a noermal string 
-   //string name[] = {"ritik", "dnyanesh", "Vihwaa"};
+   string name = "Ritik";
+   // this is a normal string 
+   string name[] = {"ritik", "dnyanesh", "Vihwaa"};
    //this is an array now 
    //index starts from 0 .... Each element has an index number
    //it starts from 0 cuz computer starts always from 0.
@@ -20,15 +20,16 @@ int main(){
    //now for displaying this we will not write each thing repeatadly
    //USE FOR LOOP
 
-//    for(int i=0; i < sizeof(car)/sizeof(car[0]); i++){
-//         cout << car[i] <<'\n';    
-// }
+   for(int i=0; i < sizeof(car)/sizeof(car[0]); i++){
+        cout << car[i] <<'\n';    
+    }
+
 /*here sizeof() this operator is used this is to get the size of the array 
 it is mostly used in loop for getting number of elements*/
 //FOR EACH LOOP
    for( string cars : car){
     cout << cars << '\n';
-
    }
+
     return 0;
 }
