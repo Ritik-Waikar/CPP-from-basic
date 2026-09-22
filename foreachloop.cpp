2854b7472bd2b87147cf6d3f974a9cd846eb38eb
Ritik-Waikar <rwcool14@gmail.com>
2026-09-22 23:21:27 +0530

    changes

--- foreachloop.cpp
+++ foreachloop.cpp
@@ -10,7 +10,11 @@ int main(){
         cout << grade << '\n';
     // for every grade in grades lets display each grade 
     }
-
-
+    // this is used to shorten the basic iteration loop but go with basics 
+    // this is less flexible because we cannot access the index of the array in this loop
+    for(int i = 0; i < sizeof(grades)/sizeof(grades[0]); i++){
+        cout << grades[i] << '\n';
+    }
+// this is the basic for loop which is more flexible because we can access the index of the array in this loop
     return 0;
 }
